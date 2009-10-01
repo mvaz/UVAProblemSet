8f200e93c856703a385afc49c8b8267b39eb3717
Miguel Vaz <mvaz@dei.uminho.pt>
2009-10-02 01:44:55 +0200

    pile_over is implemented

--- 101.cpp
+++ 101.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <list>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ class Table {
 public:
 	
 	Table(int numberOfBins)
-	:config(numberOfBins), numberOfBins( numberOfBins ), positions(numberOfBins)
+	:config(numberOfBins), numberOfBins( numberOfBins ), positions(numberOfBins), buffer()
 	{
 		for(size_t i = 0; i < this->numberOfBins; ++i) {
 			config[i].push_back(i);
@@ -21,8 +22,8 @@ public:
 	
 	void print()
 	{
-	    for(size_t i = 0; i < this->numberOfBins; ++i)
-	    {
+		for(size_t i = 0; i < this->numberOfBins; ++i)
+		{
 			cout << i << ":";
 			list<int>::iterator it = config[i].begin();
 			while (it != config[i].end())
@@ -31,15 +32,32 @@ public:
 				++it;
 			}
 			cout << "\n";
-	    }
+		}
+		// 
+		for (size_t i=0; i< positions.size(); cout << " " << positions[i++]);
 	}
 	// void move_onto( int block1, int block2 );
 	// void move_over( int block1, int block2 );
 	// void pile_onto( int block1, int block2 );
-    void pile_over( int block1, int block2 ) {
-		// find block1
+	void pile_over( int block1, int block2 ) {
+		int p1 = positions[block1];
+		int p2 = positions[block2];
 		
-		// find block2
+		//
+		list<int>::iterator it = config[p1].begin();
+		do {
+			int elem = config[p1].back();
+			config[p1].pop_back();
+			buffer.push(elem);
+			++it;
+		} while ( *it != p1 && it != config[p1].end() );
+
+		while ( !buffer.empty() ) {
+			int elem = buffer.top();
+			buffer.pop();
+			config[p2].push_back(elem);
+			positions[elem] = p2;
+		}
 	}
 
 private:
@@ -48,6 +66,7 @@ private:
 
 	vector< list<int> > config;
 	vector<int> positions;
+	stack<int>  buffer;
 
 
 	void push( int block, int position) {
@@ -84,7 +103,7 @@ int main() {
 		// cout << com1 << ":" << x1;
 	}
 
-    // print the lists
+	// print the lists
 	table.print();
 
 	exit(0);
