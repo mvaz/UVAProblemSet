6b33cae60bceac51e6d8fc1b320ad56487b38773
Miguel Vaz <mvaz@dei.uminho.pt>
2009-10-02 23:04:37 +0200

    ...

--- 102.cpp
+++ 102.cpp
@@ -5,17 +5,35 @@
 
 using namespace std;
 
+#define BROWN 0
+#define GREEN 1
+#define CLEAR 2
+#define NUMBER_COLOURS 3
+
+
+int cost( int *bin, int COLOUR)
+{
+	int cost = 0;
+	for(size_t i = 0; i < NUMBER_COLORS; ++i)
+	{
+		if ( i == COLOUR )
+			cost += bin[i];
+	}
+	return cost;
+}
+
 
 int main() {
 
 	// istream is;
-	int b1, b2, b3, g1, g2, g3, c1, c2, c3;
+	int i = 0;
+	int one[NUMBER_COLOURS], two[NUMBER_COLOURS], three[NUMBER_COLOURS];
 	
 	// get the pairs of integers
-	while( scanf("%d %d %d %d %d %d %d %d %d\n", &b1, &g1, &c1,
-												 &b2, &g2, &c2,
-												 &b3, &g3, &c3) == 9) {
-		
+	while( scanf("%d %d %d %d %d %d %d %d %d\n", &one[BROWN], &one[GREEN], &one[CLEAR],
+												 &two[BROWN], &two[GREEN], &two[CLEAR],
+												 &three[BROWN], &three[GREEN], &three[CLEAR]) == 9) {
+		cout << "line" << ++i << ":" << three[GREEN] << endl;
 	}
 
 	exit(0);
