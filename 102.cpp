6b68798999a91c1efcde06d3127c204d4ecc55fc
Miguel Vaz <mvaz@dei.uminho.pt>
2009-10-02 15:26:29 +0200

    added 102

---
+++ 102.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+	// istream is;
+	int b1, b2, b3, g1, g2, g3, c1, c2, c3;
+	
+	// get the pairs of integers
+	while( scanf("%d %d %d %d %d %d %d %d %d\n", &b1, &g1, &c1,
+												 &b2, &g2, &c2,
+												 &b3, &g3, &c3) == 9) {
+		
+	}
+
+	exit(0);
+}
